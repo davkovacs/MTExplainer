8e57c63f1c3e0fc20ccd2e94f971790bc4a7558d
William McCorkindale <wjm41@cam.ac.uk>
2020-03-10 11:33:25 +0000

    added -baseline argument, nicer logging at the end

--- experiments/attribute_test.sh
+++ experiments/attribute_test.sh
@@ -3,11 +3,12 @@
 dataset=MIT_mixed_augm
 save_model=../trained_model/
 source_txt=./sear.txt
+baseline_txt=./baseline.txt
 target_txt1=./sear_tgt.txt
 target_txt2=./sear_tgt2.txt
 
 python ../translate_IG.py -model ${save_model}${dataset}_last20_average.pt \
-	-src ${source_txt} -tgt ${target_txt1} -tgt2  ${target_txt2}\
+	-src ${source_txt} -baseline ${baseline_txt} -tgt ${target_txt1} -tgt2  ${target_txt2}\
 	-output predictions_on_sear_with_tgts.txt \
 	-batch_size 64 -replace_unk -max_length 200 -n_best 1 -verbose
 # sed -i "s/ //g"  predictions_on_sear_with_tgt2.txt

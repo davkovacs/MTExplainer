1ba42f589e83da9c6b2273b00f175527108bd6fd
wjm41 <wjm41@cam.ac.uk>
2020-05-28 01:01:58 +0800

    finalised fast unlearning

--- onmt/bin/translate_unlearn_fast.py
+++ onmt/bin/translate_unlearn_fast.py
@@ -14,111 +14,46 @@ from onmt.utils.misc import split_corpus
 from onmt.utils.parse import ArgumentParser
 from onmt.utils.logging import init_logger
 from onmt.utils.loss import build_loss_compute
+from onmt.inputters.inputter import build_dataset_iter
 from onmt.translate.translator_gold import build_translator
 import tqdm
 
 
-class GoldScorer(nn.Module):
-    # nn to produce the probability of tgt
-    def __init__(self, opt):
-        super(GoldScorer, self).__init__()
-        self.gold_scorer = TranslateGold(opt)
-
-    def forward(self, src_embed):
-        return self.gold_scorer(src_embed)
-
-def train(steps, opt, unlearn=False):
+def train(translator, batch, opt):
+    translator.model.train() # set to train mode
     #load training data in opt as batch?
+    src, src_lengths = batch.src
+    tgt = batch.tgt
 
-    loss_func = build_loss_compute(model, tgt_field, opt)
+    tgt_field = dict(translator.fields)["tgt"].base_field
+    loss_func = build_loss_compute(translator.model, tgt_field, opt)
+    params = [p for p in translator.model.parameters() if p.requires_grad]
+
     optimizer = optim.SGD(params, lr=opt.learning_rate)
-    for i in range(steps):
-        #calculate loss, do step
+    for i in range(opt.train_steps):
+        optimizer.zero_grad()
 
-        src, src_lengths = batch.src
-        outputs, attns = model(src, batch.tgt, src_lengths, bptt=False,
-                                    with_align=self.with_align)
+        outputs, attns = translator.model.forward(src, tgt, src_lengths, bptt=False,
+                                                  with_align=False)
         loss, batch_stats = loss_func(
             batch,
             outputs,
             attns,
             normalization=1, # should be fine? see 159 of trainer.py
-            shard_size=1, #should set to just 1?
-            unlearn=unlearn)
+            shard_size=1,
+            unlearn=True)
         if loss is not None:
-            #zero grad? #accum gradients?
             optimizer.backward(loss)
             loss.backward()
         else:
-            print('None Loss found!')
-        optim.step() #?
-        continue
-
-class TranslateGold(object):
-    # functor returning probability of tgt given src_embed
-    def __init__(self, opt):
-        self.opt = opt
-
-    def __call__(self, src_embed, gen_hidden_states=False):
-        translator = build_translator(self.opt, report_score=True)
-        src_shards = split_corpus(self.opt.src, self.opt.shard_size)
-        tgt_shards = split_corpus(self.opt.tgt, self.opt.shard_size)
-        shard_trips = zip(src_shards, tgt_shards)
-
-        for i, (src_shard, tgt_shard) in enumerate(shard_trips):
-            return translator.translate_gold_diff(
-                src=src_shard,
-                tgt=tgt_shard,
-                src_dir=self.opt.src_dir,
-                batch_size=self.opt.batch_size,
-                batch_type=self.opt.batch_type,
-                attn_debug=self.opt.attn_debug,
-                align_debug=self.opt.align_debug,
-                src_embed=src_embed)
-
-
-def translate(opt):
-    """ Returns source embeddings"""
-
-    ArgumentParser.validate_translate_opts(opt)
-    logger = init_logger(opt.log_file)
-
-    translator = build_translator(opt, report_score=True)
-    src_shards = split_corpus(opt.src, opt.shard_size)
-
-    # Loop for src_embedding
-    for i, src_shard in enumerate(src_shards):
-        src_data = {"reader": translator.src_reader, "data": src_shard, "dir": opt.src_dir}
-        _readers, _data, _dir = inputters.Dataset.config(
-            [('src', src_data)])
-
-        data = inputters.Dataset(
-            translator.fields, readers=_readers, data=_data, dirs=_dir,
-            sort_key=inputters.str2sortkey[translator.data_type],
-            filter_pred=translator._filter_pred
-        )
-
-        data_iter = inputters.OrderedIterator(
-            dataset=data,
-            device=translator._dev,
-            batch_size=opt.batch_size,
-            batch_size_fn=translator.max_tok_len if opt.batch_type == "tokens" else None,
-            train=False,
-            sort=False,
-            sort_within_batch=True,
-            shuffle=False
-        )
-
-        for batch in data_iter:
-            src, src_lengths = batch.src
-            src_embed = translator.model.encoder.embed(src, src_lengths)
-
-    return src_embed
-
+            print('Oh no, Loss is None!')
+        optim.step()
 
 def _get_parser():
-    parser = ArgumentParser(description='translate.py')
+    parser = ArgumentParser(description='translate_unlearn_fast.py')
     opts.config_opts(parser)
+    opts.model_opts(parser)
+    opts.train_opts(parser)
     opts.translate_opts(parser)
     return parser
 
@@ -127,16 +62,45 @@ def main():
     parser = _get_parser()
     opt = parser.parse_args()
 
-    src_embed = translate(opt)  # Get source embeddings
+    translator = build_translator(opt, report_score=True) # TODO check report_score
 
-    gold_scorer = GoldScorer(opt)
-    score = gold_scorer(src_embed)
-    try:
-        score_list = np.load(opt.score_file)
-    except FileNotFoundError:
-        score_list=np.ndarray([])
-    score_list = np.append(score_list, score.detach().numpy()[0])
-    np.save(opt.score_file, score_list)
+    # TODO delete this if the assert is working
+    checkpoint = torch.load(opt.train_from,
+                            map_location=lambda storage, loc: storage)
+    fields = checkpoint['vocab']
+
+    assert fields==translator.fields
+    train_iter = build_dataset_iter("train", translator.fields, opt)
+
+    score_list = []
+    for batch in tqdm(train_iter):
+        src, src_lengths = batch.src
+        tgt = batch.tgt
+        translator_copy = translator.copy() # TODO check that this makes a proper copy (no pointers!)
+
+        train(translator_copy, batch, opt) # should update model weights
+        src_embed = translator_copy.model.encoder.embed(src, src_lengths)
+
+        # Set to translate mode
+        translator_copy.model.eval()
+        translator_copy.model.generator.eval()
+
+        src_shards = split_corpus(opt.src, opt.shard_size)
+        tgt_shards = split_corpus(opt.tgt, opt.shard_size)
+        shard_trips = zip(src_shards, tgt_shards)
+
+        for i, (src_shard, tgt_shard) in enumerate(shard_trips):
+            score = translator.translate_gold_diff(
+                src=src_shard,
+                tgt=tgt_shard,
+                src_dir=opt.src_dir,
+                batch_size=opt.batch_size,
+                batch_type=opt.batch_type,
+                attn_debug=opt.attn_debug,
+                align_debug=opt.align_debug,
+                src_embed=src_embed)
+        score_list.append(score.detach()[0])
+    np.save(opt.score_file, score_list) # TODO make sure to choose different score_file to avoid overwriting previous results
 
 if __name__ == "__main__":
     main()

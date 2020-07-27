a1c946eb81f147ff7c76f46931f30684bac1b091
D.P. Kovacs <dpk25@login-e-6.data.cluster>
2020-07-27 11:40:57 +0100

    some experiments run

--- scripts/average_models.sh
+++ scripts/average_models.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-dataset=MIT_mixed_augm
-save_model=/home/dpk25/rds/hpc-work/MolecularTransformer2_models/checkpoints/
-model_list=${save_model}last20/
+model_list=/home/dpk25/rds/hpc-work/pistachio/checkpoints/last_20/
 
 python ../tools/average_models.py -models ${model_list}*.pt \
-	-output ${save_model}${dataset}_last20_average.pt
+	-output ${model_list}pist_last20_avg.pt

fb3bb9084797685ff6e54a24576bbb7f93988196
D.P. Kovacs <dpk25@login-e-6.data.cluster>
2020-07-29 17:56:50 +0100

    tidy up experiments folder

--- scripts/average_models.sh
+++ scripts/average_models.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-model_list=/home/dpk25/rds/hpc-work/pistachio/checkpoints/last_20/
+model_list=/home/dpk25/rds/hpc-work/pistachio/checkpoints/last_10/
 
 python ../tools/average_models.py -models ${model_list}*.pt \
-	-output ${model_list}pist_last20_avg.pt
+	-output ${model_list}pist_last10_avg.pt

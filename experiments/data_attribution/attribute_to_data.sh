3aca4395de976f457ce51681872940a525e8a937
D.P. Kovacs <dpk25@login-e-6.data.cluster>
2020-07-29 11:29:53 +0100

    data attribution code improvement

--- experiments/data_attribution/attribute_to_data.sh
+++ experiments/data_attribution/attribute_to_data.sh
@@ -5,6 +5,7 @@ data_dir=/home/dpk25/MolecularTransformer2/data/data/MIT_mixed_clean/
 model=/home/dpk25/rds/hpc-work/MolecularTransformer2_models/checkpoints/MIT_mixed_clean_augm/MIT_mixed_clean_augm_last20_avg.pt
 hidden_array=/home/dpk25/rds/hpc-work/hidden_states/hidden_avg.npy
 train_rxs=MIT_clean_train.txt
+n_return=5
 
 python data_attribution_vectorized.py -model ${model} -input ${inp_rx} -hiddens ${hidden_array} \
-		-n_match 5 -dataset ${train_rxs}
+		-n_match ${n_return} -dataset ${train_rxs}

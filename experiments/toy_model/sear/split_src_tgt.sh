a046da7fd8615227025c7aa54b611ac328672083
wjm41 <wjm41@cam.ac.uk>
2020-08-17 21:47:39 +0100

    included shuffling of training set

--- experiments/toy_model/sear/split_src_tgt.sh
+++ experiments/toy_model/sear/split_src_tgt.sh
@@ -7,6 +7,10 @@ sed 's/.*>>//g' train.txt > tgt-train.txt
 sed 's/.*>>//g' val.txt > tgt-val.txt
 sed 's/.*>>//g' test.txt > tgt-test.txt
 
+paste -d ':' src-train.txt tgt-train.txt | sort -R > train.txt
+sed 's/.*://g' train.txt > tgt-train.txt
+sed 's/:.*//g' train.txt > src-train.txt
+
 rm train.txt
 rm val.txt
 rm test.txt

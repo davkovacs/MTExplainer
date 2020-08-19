4fe95fc5bc9161f3af56c655125549f1eae5d428
D.P. Kovacs <dpk25@login-e-9.data.cluster>
2020-08-19 18:52:26 +0100

    new conv tests

--- scripts/toy_model/sear_convergence/test_small.sh
+++ scripts/toy_model/sear_convergence/test_small.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 datadir=/home/dpk25/MolecularTransformer2/experiments/toy_model/sear/
-model_list=/home/dpk25/rds/hpc-work/toy_model/sear_models_small_conv/
+model_list=/home/dpk25/rds/hpc-work/toy_model/sear_models_supBiased_conv/
 
 for i in {1..10}
 do
-    j=$((312*$i))
+    j=$((76*$i))
     python ../../../translate.py -model ${model_list}toy_model_step_$j.pt \
         -src ${datadir}src-benchmark.txt \
-        -output ./conv_small_$j.txt \
+        -output ./conv_supBiased_$j.txt \
         -batch_size 64 -replace_unk -max_length 200 -n_best 1
 done 

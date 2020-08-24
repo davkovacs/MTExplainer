846fc02b562d21df1a76a93f66575a8c87897411
DavKovacs <davkovacs97@gmail.com>
2020-08-24 11:33:47 +0100

    further clean-up

--- experiments/lev_vs_mol_transf/translate.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-model=/home/dpk25/rds/hpc-work/MolecularTransformer2_models/checkpoints/MIT_mixed_clean_augm/MIT_mixed_clean_augm_last20_avg.pt
-source_txt=test_src.txt
-
-python ../../translate.py -model ${model} \
-	-src ${source_txt} \
-	-output preds_mol_t_noBeam.txt --beam_size 1 \
-	-replace_unk -max_length 200 -n_best 1 -batch_size 128 -gpu -1
--- experiments/lev_vs_mol_transf/translate_train.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-model=/home/dpk25/rds/hpc-work/MolecularTransformer2_models/checkpoints/MIT_mixed_clean_augm/MIT_mixed_clean_augm_last20_avg.pt
-source_txt=src-train.txt
-
-python ../../translate.py -model ${model} \
-	-src ${source_txt} \
-	-output preds_mol_t_train.txt \
-	-replace_unk -max_length 200 -n_best 1 -batch_size 256 -gpu -1

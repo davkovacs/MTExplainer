61fa3262941c259a5ad048bc760d9388da19990d
DavKovacs <davkovacs97@gmail.com>
2020-08-24 11:43:31 +0100

    clean-up

--- scripts/preprocess_vocab.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-dataset=../data/data/MIT_mixed_clean_augm/unlearn4/
-python ../preprocess.py -train_src ${dataset}src-train.txt \
-	-train_tgt ${dataset}tgt-train.txt \
-	-save_data ${dataset} \
-	-src_seq_length 1000 -tgt_seq_length 1000 \
-	--src_vocab ../data/data/MIT_mixed_clean_augm/.vocab.pt --share_vocab
